3b969d9fd8f8c2815492b5ed161c4db61b1a7e89
Richard Sipes <rksipes@gmail.com>
2017-01-13 23:56:44 -0800

    feat: add angular directives

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('store', []);
+// wrapping JS in a closure is a good habit
+(function() {
+    var app = angular.module('store', []);
+
+    // The controller is attached to (inside) our app.
+    app.controller('StoreController', function(){
+        this.product = gem;
+    });
+
+    var gem = {
+        name: 'Dodecahedron',
+        price: 2.95,
+        description: '. . .',
+    }
+
+})();
